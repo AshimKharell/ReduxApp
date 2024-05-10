a0d7693ff24d59c05a26d1bd14103535383acdde
kharelashim <144309749+kharelashim@users.noreply.github.com>
2024-05-10 10:06:01 +0545

    Started the redux app with shop component and navbar component with some button

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Buy Sunglasses 1000 RS</h2>
+      <button className="btn btn-primary mx-2">-</button>
+        Add item to the cart
+      <button className="btn btn-primary mx-2">+</button>
+
+    </div>
+  )
+}
+
+export default Shop
