ac671a5b7470bfa1b7c2c5dc42b9dc066c3974dd
kharelashim <144309749+kharelashim@users.noreply.github.com>
2024-05-10 14:27:32 +0545

    added a reducer and action creators

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-      <h2>Buy Sunglasses 1000 RS</h2>
+      <h2>Deposite/Withdraw Money</h2>
       <button className="btn btn-primary mx-2">-</button>
-        Add item to the cart
+       Update Balance
       <button className="btn btn-primary mx-2">+</button>
 
     </div>
